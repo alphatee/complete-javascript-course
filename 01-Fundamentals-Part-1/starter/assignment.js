914ee3b270e0dfef68afaf946b98e82bf1ace3bc
Daniel Herrera <dher426@wgu.edu>
2024-12-28 21:44:42 -0800

    variables

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,7 @@
+let country = "France";
+let continent = "Europe";
+let population = 68170000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,12 @@
+let js = 'amazing';
+console.log(2 + 8 - 8);
+console.log(js);
+
+let person = 'Daniel';
+let anotherPerson = "Daniel";
+let anotherPersonAgain = "Daniel";
+let PI = 3.1415;
+
+console.log(PI);
+console.log(person + " " + anotherPerson);
+
