3146a789afcdbcc243e138e6fa8c0427bc72b96d
Daniel Herrera <dher426@wgu.edu>
2025-04-22 14:51:45 -0700

    I learned basics

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js = 'amazing';
 console.log(2 + 8 - 8);
 console.log(js);
@@ -10,3 +11,57 @@ let PI = 3.1415;
 console.log(PI);
 console.log(person + " " + anotherPerson);
 
+console.log(typeof true)
+console.log(typeof null)
+
+let age = 30;
+age = 31;
+
+const word = 'free'
+console.log(word)
+
+let x, y;
+x = y = 25 - 10 - 5
+console.log(x)
+console.log(y)
+
+const firstName = 'Daniel'
+const job = 'developer';
+const birthYear = 1997;
+const year = 2025;
+
+const daniel = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + '!';
+console.log(daniel)
+
+const danielNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`
+console.log(danielNew)
+console.log(`a regular \n string ....`)
+
+console.log(`a
+regular
+string ....`)
+
+console.log(`hello ❤️`) // windows + '.'
+
+
+const inputYear = '1997'
+console.log(Number(inputYear));
+
+console.log(typeof String(23), typeof 23); // string, number
+
+console.log('2' + '2');
+
+// type safety
+let n = '1' + 1;
+n = n - 1;
+console.log(n);
+
+console.log('10' - '4' - '3' - 2 + '5') // '+' operator converts numbers to a string when there is a type coercion
+
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean(''));
+console.log(Boolean(NaN));
+console.log(Boolean(null));
+*/
